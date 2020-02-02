e991fb12e6548d27accefeb7dc13388b7e3b39cc
NechitaRobert <RobertMihai2804@gmail.com>
2020-02-02 23:24:54 +0200

    Add files via upload

---
+++ Exercitii_recapitulative_1/src/Exercitiul_12.java
@@ -0,0 +1,63 @@
+
+// Scrieti in java in care sa afiseze valoarea maxima si minima dintr-o matrice
+
+import java.util.Scanner;
+
+public class Exercitiul_12 {
+
+	public static void main(String args[])
+	{
+		int linie;
+		int coloana;
+		
+		Scanner valoriTastatura = new Scanner(System.in);
+		System.out.println("Specificati cate linii sa aiba matricea ");
+		linie = valoriTastatura.nextInt();
+		System.out.println("Speciicati cate coloane sa aiba matricea ");
+		coloana = valoriTastatura.nextInt();
+		
+		int matrice[][] = new int[linie][coloana];
+		for (int i = 0; i < linie; i++)
+		{
+			for (int j = 0; j < coloana; j++)
+			{
+				System.out.println("Specificati valoarea in locatia " + i + j);
+				matrice[i][j] = valoriTastatura.nextInt();
+			}
+		}
+		
+		for (int i = 0; i < linie; i++)
+		{
+			for (int j = 0; j < coloana; j++)
+			{
+				System.out.print("[" + matrice[i][j] + "]");
+			}
+			System.out.println();
+		}
+		
+		int minim = matrice[0][0];
+		int maxim = matrice[0][0];
+		
+		for (int i = 0; i < linie; i++)
+		{
+			for (int j = 0; j < coloana; j++)
+			{
+				if( matrice[i][j] < minim )
+				{
+					minim = matrice[i][j];
+				}
+				
+				if (matrice [i][j] > maxim)
+				{
+					maxim = matrice[i][j];
+				}
+				
+			}
+		}
+		
+		System.out.println("Valoarea minima a vectorului este " + minim);
+		System.out.println("Valoarea maxima a vectorului este " + maxim);
+		
+	}
+	
+}
--- Exercitii_recapitulative_1/src/Exercitiul_4.java
+++ Exercitii_recapitulative_1/src/Exercitiul_4.java
@@ -1,56 +1,65 @@
+
 import java.util.Scanner;
 
 public class Exercitiul_4 {
 
-    public static void main(String args[])
-    {
-        int matrice[][] = new int[3][3];
-        int rand = matrice.length;
-        System.out.println(rand);
-        int coloana = matrice[0].length;
-        System.out.println(coloana);
+	public static void main(String args[])
+	{
+		int matrice[][] = new int[2][5];
+		int rand = matrice.length;
+		System.out.println(rand);
+		int coloana = matrice[0].length;
+		System.out.println(coloana);
+		
+		Scanner valoriTastatura = new Scanner(System.in);
+		
+			
+		for (int i = 0; i < rand; i++)
+		{
+			for (int j = 0; j < coloana; j++)
+			{
+				System.out.println("Valoare la coordonatele " + i + j);
+				matrice[i][j] = valoriTastatura.nextInt();
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < rand; i++)
+		{
+			for(int j = 0; j < coloana; j++)
+			{
+				System.out.print(matrice[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		// nota fa pe foaie cu coordonatele
+		int matriceInversa[][] = new int[rand][coloana];
+		
+		for (int i = rand - 1 ; rand >= 0; i -- )
+		{
+			for (int j = coloana - 2 ; coloana >= 0; j-- )
+			{
+				matriceInversa[rand-1-i][coloana-1-j] = matrice[i][j];
+				
+			}
 
-        Scanner valoriTastatura = new Scanner(System.in);
-
-
-        for (int i = 0; i < rand; i++)
-        {
-            for (int j = 0; j < coloana; j++)
-            {
-                System.out.println("Valoare la coordonatele " + i + j);
-                matrice[i][j] = valoriTastatura.nextInt();
-            }
-            System.out.println();
-        }
-
-        for (int i = 0; i < rand; i++)
-        {
-            for(int j = 0; j < coloana; j++)
-            {
-                System.out.print(matrice[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        // nota fa pe foaie cu coordonatele
-        int matriceInversa[][] = new int[rand][coloana];
-
-        for (int i = 0; i < rand; i++) {
-            for (int j = 0; j < coloana; j++) {
-                matriceInversa[i][j] = matrice[rand-i-1][coloana-j-1];
-            }
-        }
-
-        System.out.println("Matrice inversa:");
-        for (int i = 0; i < rand; i++)
-        {
-            for(int j = 0; j < coloana; j++)
-            {
-                System.out.print(matriceInversa[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-    }
-
+		}
+		
+		
+		for (int i = 0; i < rand; i++)
+		{
+			for (int j = 0; j < coloana; j++)
+			{
+				System.out.print(matriceInversa[i][j] + " ");
+				
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+	}
+	
 }
